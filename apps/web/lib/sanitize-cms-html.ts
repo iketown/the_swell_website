d3910f889d08e647729938a4a545336c8bded639
gbuomprisco <giancarlopsk@gmail.com>
2026-04-18 14:46:01 +0800

    chore: update isomorphic-dompurify to version 3.9.0 and bump package version to 3.1.14

--- apps/web/lib/sanitize-cms-html.ts
+++ apps/web/lib/sanitize-cms-html.ts
@@ -1,10 +1,19 @@
-import DOMPurify from 'isomorphic-dompurify';
+import { sanitize } from 'isomorphic-dompurify';
 
-const CMS_EXCERPT_ALLOWED_TAGS = ['a', 'br', 'em', 'li', 'ol', 'p', 'strong', 'ul'];
+const CMS_EXCERPT_ALLOWED_TAGS = [
+  'a',
+  'br',
+  'em',
+  'li',
+  'ol',
+  'p',
+  'strong',
+  'ul',
+];
 const CMS_EXCERPT_ALLOWED_ATTR = ['href', 'rel', 'target'];
 
 export function sanitizeCmsExcerptHtml(html: string) {
-  return DOMPurify.sanitize(html, {
+  return sanitize(html, {
     ALLOWED_TAGS: CMS_EXCERPT_ALLOWED_TAGS,
     ALLOWED_ATTR: CMS_EXCERPT_ALLOWED_ATTR,
   });
